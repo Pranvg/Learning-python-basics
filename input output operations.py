ccb10435e53359e2b61d1dabdb3dd46c10542fc8
Pranav Gurle <137593348+Pranvg@users.noreply.github.com>
2026-09-17 11:26:35 +0530

    Uncommented code for square and number processing

--- input output operations.py
+++ input output operations.py
@@ -76,15 +76,15 @@ else:
 
 
 
-# sq = [ i*i for i in range(6) if i%2 != 0]
+sq = [ i*i for i in range(6) if i%2 != 0]
 
-# print(sq)
+print(sq)
 
-# nums = [-2, -3, 3, 4, -1, 7]
+nums = [-2, -3, 3, 4, -1, 7]
 
-# nums = [0 if val < 0 else val for val in nums]
+nums = [0 if val < 0 else val for val in nums]
 
-# print(nums)
+print(nums)
 
 
 
